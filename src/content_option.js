05f392b46e541971fa23253aa7ef28e9527511a8
Strike2-ux <13paullevy@gmail.com>
2024-12-19 18:07:49 -0300

    intentando

---
+++ src/content_option.js
@@ -0,0 +1,158 @@
+const logotext = 'Paul Levy';
+const meta = {
+  title: 'Paul Levy Diaz',
+  description: 'Soy Paul Levy un científico informático trabajando en Chile',
+};
+
+const introdata = {
+  title: 'Soy  Paul Levy',
+  animated: {
+    first: 'Amo vivir',
+    second: 'Armo paginas web',
+    third: 'Creo aplicaciones',
+    fourth: 'Trabajo en equipo',
+  },
+  description:
+    'Soy un ingeniero en administración con mención en marketing, puedo construir paginas web con herramientas o con código puro, experto en atención al cliente, trabajo presencial & remoto.',
+  your_img_url: 'https://i.ibb.co/7bfvKR6/20241219-125429-1probando.jpg',
+};
+
+const dataabout = {
+  title: 'Sobre mí',
+  aboutme:
+    'Ingeniero, Compañero, Aliado, Amigo, un muy buen general o soldado, dispuesto a trabajar en equipo, buscar el mal menor y el bien mayor siempre. Experiencia en la agricultura ya que vivo en una zona rural y no me menosprecien.',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Socio',
+    where: 'Levy y Asociados SPA',
+    date: '2021',
+  },
+  {
+    jobtitle: 'Ejecutivo Call-Center',
+    where: 'Konecta Ltda',
+    date: '2019',
+  },
+  {
+    jobtitle: 'Ejecutivo de Ventas',
+    where: 'AVAN-C',
+    date: '2018',
+  },
+];
+
+const skills = [
+  {
+    name: 'Python',
+    value: 90,
+  },
+  {
+    name: 'Django',
+    value: 85,
+  },
+  {
+    name: 'Javascript',
+    value: 80,
+  },
+  {
+    name: 'React',
+    value: 60,
+  },
+  {
+    name: 'Jquery',
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: 'Diseño UI & UX',
+    description:
+      'Puedo diseñar todo tipo de productos, soy un ingeniero en administración y diseñador grafico a la vez, diseño de conceptos y productos.',
+  },
+  {
+    title: 'Aplicaciones móviles',
+    description:
+      'Podemos trabajar en equipo o en solo, desarrollo de aplicaciones en Android y IOS, también podemos hacer aplicaciones a medida de lo que ustedes soliciten.',
+  },
+  {
+    title: 'Diseño Wordpress',
+    description:
+      'WordPress, Magento, Shopify o cualquier CMS, generalmente prefiero hacer las páginas con código puro, pero a petición de los clientes puedo usar las herramientas que desean sin problemas.',
+  },
+];
+
+const dataportfolio = [
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    description: 'Una pagina web diseñada para un laboratorio.',
+    link: 'https://strike2-ux.github.io/chemicallaboratory/',
+  },
+  {
+    img: 'https://picsum.photos/400/800/?grayscale',
+    description: 'Una pagina web de viajes alrededor de chile.',
+    link: 'https://strike2-ux.github.io/viajeschile/',
+  },
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    description: 'Un portafolio prototipo en inglés.',
+    link: 'https://strike2-ux.github.io/portfolio/',
+  },
+  {
+    img: 'https://picsum.photos/400/600/?grayscale',
+    description: '100 días de programación en Python consecutivos.',
+    link: 'https://github.com/Strike2-ux/100-days-of-code',
+  },
+  {
+    img: 'https://picsum.photos/400/300/?grayscale',
+    description:
+      'Un portafolio hecho con el propósito de entrenar habilidades.',
+    link: 'https://strike2-ux.github.io/entrenamiento/',
+  },
+  {
+    img: 'https://picsum.photos/400/700/?grayscale',
+    description: 'Un perfil de desarrollo de portafolio con react.',
+    link: 'https://dorota1997.github.io/react-frontend-dev-portfolio/',
+  },
+
+  {
+    img: 'https://picsum.photos/400/600/?grayscale',
+    description: 'Mas portafolios de react para mejorar el perfil en redes.',
+    link: 'https://reactportfoliotemplate.paytonpierce.dev/',
+  },
+  {
+    img: 'https://picsum.photos/400/300/?grayscale',
+    description: 'Una calculadora hecha con React.',
+    link: '#',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'paullevydiaz@gmail.com',
+  YOUR_FONE: '(+56) 9-65933840',
+  description:
+    'No sea tímido o tímida, puede llamarme, enviarme WhatsApp y si quiere formalidad enviarme un correo, le responderé en la brevedad.',
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: 'service_s9druor',
+  YOUR_TEMPLATE_ID: 'template_ax101mc',
+  YOUR_USER_ID: '61Wuu4K42pJCnPm71',
+};
+
+const socialprofils = {
+  github: 'https://github.com/Strike2-ux',
+  facebook: 'https://www.facebook.com/profile.php?id=61570036600750',
+  linkedin: 'https://www.linkedin.com/in/eldeveloper/',
+  twitter: 'https://x.com/paullevydiaz',
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
